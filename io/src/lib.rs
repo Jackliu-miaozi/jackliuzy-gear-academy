bc1746450a303ac54a8b78e9786987a5b96df216
jackliu <lzyujn@gmail.com>
2024-08-04 13:30:49 +0800

    提交作业

---
+++ io/src/lib.rs
@@ -0,0 +1,85 @@
+#![no_std]
+
+use gmeta::{InOut, In, Out, Metadata};
+use gstd::prelude::*;
+
+pub struct PebblesMetadata;
+
+impl Metadata for PebblesMetadata {
+    type Init = In<PebblesInit>;
+    type Handle = InOut<PebblesAction, PebblesEvent>;
+    type State = Out<GameState>;
+    type Reply = ();
+    type Others = ();
+    type Signal = ();
+}
+
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub struct PebblesInit {
+    pub difficulty: DifficultyLevel,
+    pub pebbles_count: u8,
+    pub max_pebbles_per_turn: u8,
+}
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo, PartialEq, Eq)]
+pub enum DifficultyLevel {
+    #[default]
+    Easy,
+    Hard,
+}
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum PebblesAction {
+    Turn(u8),
+    GiveUp,
+    Restart {
+        difficulty: DifficultyLevel,
+        pebbles_count: u8,
+        max_pebbles_per_turn: u8,
+    },
+}
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum PebblesEvent {
+    CounterTurn(u8),
+    Won(Player),
+    WrongTurn,
+    //取走的石子数大于剩余石子数
+    NewRound,
+    //新的回合
+    End,
+    //游戏结束
+}
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub enum Player {
+    #[default]
+    User,
+    Program,
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum StateQuery {
+    All,
+    Winner,
+    CounterTurn,
+    PebblesRemaining,
+}
+
+#[derive(Encode, Decode, TypeInfo)]
+pub enum StateReply {
+    All(GameState),
+    Winner(Option<Player>),
+    CounterTurn(u8),
+    PebblesRemaining(u8),
+}
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub struct GameState {
+    pub pebbles_count: u8,
+    pub max_pebbles_per_turn: u8,
+    pub pebbles_remaining: u8,
+    pub difficulty: DifficultyLevel,
+    pub first_player: Player,
+    pub winner: Option<Player>,
+    pub counter_turn: u8,
+}
